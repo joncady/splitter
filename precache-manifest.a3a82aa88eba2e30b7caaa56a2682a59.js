125a172e41b01c7e47810f91a110da9da74b4208
Jonathan Cady <joncady@uw.edu>
2018-12-28 13:11:28 -0800

    Updates

---
+++ precache-manifest.a3a82aa88eba2e30b7caaa56a2682a59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9e222c48e12e7894ea7",
+    "url": "/static/js/main.a9e222c4.chunk.js"
+  },
+  {
+    "revision": "1653c561203a194847d7",
+    "url": "/static/js/1.1653c561.chunk.js"
+  },
+  {
+    "revision": "a9e222c48e12e7894ea7",
+    "url": "/static/css/main.2624bec9.chunk.css"
+  },
+  {
+    "revision": "1653c561203a194847d7",
+    "url": "/static/css/1.34187ac0.chunk.css"
+  },
+  {
+    "revision": "fa2d0fdec81042c0925186b643a7570a",
+    "url": "/index.html"
+  }
+];
